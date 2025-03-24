c6215164f9b5d7806dc785ad9a3ec4785b868e45
Belozerovmsk <belozerov.msk@gmail.com>
2025-03-25 00:49:17 +0300

    need to fix daemon pgq

--- init/init_pgq.sql
+++ init/init_pgq.sql
@@ -1,5 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS pgq;
 
-select * from pgq.create_queue('error_queue');
+-- select * from pgq.create_queue('error_queue');
+--
+-- select * from pgq.register_consumer('error_queue', 'telegram_bot_consumer');
 
-select * from pgq.register_consumer('error_queue', 'telegram_bot_consumer');
+-- select * from pgq.next_batch('error_queue', 'telegram_bot_consumer');
+--
+-- select * from pgq.current_event_table('error_queue');
+
+
+-- SELECT pgq.insert_event(
+--                'error_queue',
+--                'error',
+--                '{"target": "https://example.com", "message": "Test error message"}'
+--        );
